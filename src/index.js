aad9eb7a70db58113ee4f40567617360de73a309
nigthblue <user@>
2019-03-23 22:14:55 +0300

    q3

--- src/index.js
+++ src/index.js
@@ -1,37 +1,60 @@
 module.exports = function getZerosCount(number, base) {
-	
-  // your implementation
-  var count=[];
-  var base1=base;
-  for(i=2;i<base1+1;i++){
-		while(base1%i==0){
-			count.push(i);
-			base1/=i;
-		}
-	}
-  var k=count.length;	
-  count=[...new Set(count)];
-  var summ=[];
-  var c=0;
-  for(item=0;item<count.length;item++){
-	  
-	  for(x=2;x<number+1;x++){
-		  var y=x;
-		  while((y)%count[item]==0){
-			  c++;
-			  y/=count[item];
-		  }
-	  }
-	  summ.push(c);
-	  c=0; 
-  }
-  var c = summ[0];
-    for (var i = 0; i < summ.length; i++) {
-        if (c > summ[i]) c = summ[i];
+  
+  const legendreComponents = findDividers(base);
+  return Math.floor(psevdoFac(number, legendreComponents[0]) / legendreComponents[1]);
+
+  function psevdoFac(number, divider) {
+    if (number === 0) {
+      return 0;
     }
-  if(base==Math.pow(count[0],k)){
-	  c=Math.floor(c/k);
+    return Math.floor(number / divider) + psevdoFac(Math.floor(number / divider), divider);
+  }
+
+  function findNextOdd(number) {
+    if (number % 2 === 0) {
+      return ++number;
+    }
+    return number += 2;
+  }
+    
+  function maxOf(a, b, powA, powB) {
+    if ( Math.floor(Math.pow(a, powA) / Math.pow(b, powB)) > Math.pow(b, powB) ) {
+      return [a, powA];
+    }
+    return [b, powB];
+  }
+
+  function defineDividerOfLegendre(dividers, powers) {
+    if (dividers.length == 1) {
+      return [dividers[0], powers[0]];
+    }
+    let max = maxOf(dividers[0], dividers[1], powers[0], powers[1]); 
+    for (let i = 1; i < dividers.length - 1; i++) {
+      max = maxOf(max[0], dividers[i+1], max[1], powers[i+1]); 
+    }
+    return max;
   }
   
-  return c;
+  function findDividers(number) {
+    const dividers = [],
+          powers = [];
+    let divider = 2;
+    do {
+      if (number % divider === 0) {
+        number = number / divider;
+        
+        if ( ! dividers.includes(divider) ) {
+          dividers.push(divider);
+          powers[dividers.length - 1] = 0;
+        }
+        powers[dividers.length - 1]++;
+        
+      }
+      else {
+         divider = findNextOdd(divider);
+      }
+    } while (number != 1);
+    
+    return defineDividerOfLegendre(dividers, powers);
+  }
 }
