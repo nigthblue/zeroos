27c62bd8ebb48f979b94594b478a4670ff800966
nigthblue <user@>
2019-03-23 07:03:49 +0300

    everyday

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
 module.exports = function getZerosCount(number, base) {
 	
   // your implementation
-  var count=primeCount(base);
+  var count=[];
+  for(i=2;i<base+1;i++){
+		while(base%i==0){
+			count.push(i);
+			base/=i;
+		}
+	}
+  count=[...new Set(count)];
   var summ=[];
   var c=0;
-  
   for(item=0;item<count.length;item++){
 	  for(x=2;x<number+1;x++){
 		  var y=x;
-		  while(y%item==0){
+		  while((y)%count[item]==0){
 			  c++;
-			  y/=item;
+			  y/=count[item];
 		  }
 	  }
 	  summ.push(c);
-	  c=0;  
+	  c=0; 
   }
-  c=Math.min(summ);
-  if(base==Math.pow(count[0],summ.length)){
-	  c=Math.floor(c/summ.length);
-  }
-  console.log(c);
-  return c;
-}
-function primeCount(num){
-	var count=[];
-	for(i=2;i<num+1;i++){
-		while(num%i==0){
-			count.push(i);
-			num/=i;
-		}
+  var c = summ[0];
+    for (var i = 0; i < summ.length; i++) {
+        if (c > summ[i]) c = summ[i];
+    }
+	console.log(c);
+	var perem=1;
+	for(x=0;x<count.length;x++){
+		perem*=count[x];
 	}
-	return count;
+  if(base==Math.pow(perem,summ.length)){
+	  c=Math.floor(c/count.length);
+	  console.log(c);
+  }
+  
+  return c;
 }
