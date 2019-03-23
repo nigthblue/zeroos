6ef242795e82cad3e7aadb0c543e668819f5357e
nigthblue <user@>
2019-03-23 07:45:37 +0300

    12gabs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ module.exports = function getZerosCount(number, base) {
 	
   // your implementation
   var count=[];
-  for(i=2;i<base+1;i++){
-		while(base%i==0){
+  var base1=base;
+  for(i=2;i<base1+1;i++){
+		while(base1%i==0){
 			count.push(i);
-			base/=i;
+			base1/=i;
 		}
 	}
+  var k=count.length;	
   count=[...new Set(count)];
   var summ=[];
   var c=0;
   for(item=0;item<count.length;item++){
+	  
 	  for(x=2;x<number+1;x++){
 		  var y=x;
 		  while((y)%count[item]==0){
@@ -26,14 +29,8 @@ module.exports = function getZerosCount(number, base) {
     for (var i = 0; i < summ.length; i++) {
         if (c > summ[i]) c = summ[i];
     }
-	console.log(c);
-	var perem=1;
-	for(x=0;x<count.length;x++){
-		perem*=count[x];
-	}
-  if(base==Math.pow(perem,summ.length)){
-	  c=Math.floor(c/count.length);
-	  console.log(c);
+  if(base==Math.pow(count[0],k)){
+	  c=Math.floor(c/k);
   }
   
   return c;
